41a8779e45bcf1610d3857695651c4fc64d6e106
Jordan Engstrom <jordan.engstrom@proton.me>
2026-03-23 11:32:13 -0700

    Fixes padding top

--- client/src/pages/values.tsx
+++ client/src/pages/values.tsx
@@ -29,7 +29,7 @@ const values = [
 export default function Values() {
   return (
     <Layout>
-      <section className="relative pt-10 pb-16 px-6 overflow-hidden min-h-dvh flex flex-col justify-center">
+      <section className="values-section relative overflow-hidden min-h-dvh flex flex-col justify-center">
         <LiquidGradient interactive={false} className="z-0" />
         <div className="absolute inset-0 bg-white/55 dark:bg-slate-950/60 z-[1] pointer-events-none" />
 
